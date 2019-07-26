db725b78e64168bea204f33fb0cd4957088a0baf
Mano <airscythe@gmail.com>
2019-07-26 21:05:48 +0200

    Add first test

--- src/test/java/splitter/CurlToComponentsTest.java
+++ src/test/java/splitter/CurlToComponentsTest.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CurlToComponentsTest {
+public class CurlToComponentsTest {
     @Test
-    void extractComponents() {
+    public void extractComponentsTest() {
         List<Component> componentList = CurlToComponents.extractComponents(
                 "curl -d \"param1=value1&param2=value2&param3=http://test.com\" -H \"Content-Type: application/x-www-form-urlencoded\" -X POST http://localhost:3000/data");
         assertEquals(componentList.size(), 1);
