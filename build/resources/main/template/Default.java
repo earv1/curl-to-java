084a44fa60d3c91391d9f474bec3117dca17674a
Mano <airscythe@gmail.com>
2019-07-28 14:24:08 +0200

    Start of code executor framework

--- build/resources/main/template/Default.java
+++ build/resources/main/template/Default.java
@@ -1,5 +1,12 @@
 package template;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+
+//{imports}
+
 public class Default {
 
     public static void main (String[]args) {
---
+++ src/main/java/codeexecutor/CodeExecutor.java
@@ -0,0 +1,56 @@
+package codeexecutor;
+
+import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class CodeExecutor {
+    private static String fileName = "Test";
+    public static void runCode(String codeToExecute, String variableToGrabOutputFrom) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            // IMPORTANT: Save the old System.out!
+            PrintStream old = System.out;
+            // Tell Java to use your special stream
+            System.setOut(ps);
+
+
+            JavaCompiler jc = ToolProvider.getSystemJavaCompiler();
+            StandardJavaFileManager standardFileManager = jc.getStandardFileManager(null, null, null);
+            File javaFile = new File(fileName + ".java"); //create file in current working directory
+            PrintWriter printWriter= new PrintWriter(javaFile);
+            printWriter.println(
+                    "import org.springframework.http.HttpEntity;\n" +
+                            "import org.springframework.http.ResponseEntity;\n" +
+                            "import org.springframework.web.client.RestTemplate;\n" +
+                            "import org.springframework.http.HttpMethod;\n" +
+                            "public class " + fileName + "\n"  +
+                            "{\n " +
+                            "   public static void execute(StringBuffer output)\n" +
+                            "   {\n" +
+                            codeToExecute +
+                            "output.append(" + variableToGrabOutputFrom +");"+
+                            "   }\n" +
+                            "}");
+            printWriter.close();
+            Iterable standardFileManagerJavaFileObjects = standardFileManager.getJavaFileObjects(javaFile);
+            if (!jc.getTask(null, standardFileManager, null, null, null, standardFileManagerJavaFileObjects).call()) { //compile the code
+                throw new RuntimeException("compilation failed");
+            }
+            URL[] urls = new URL[]{new File("").toURI().toURL()}; //use current working directory
+            URLClassLoader ucl = new URLClassLoader(urls);
+            Object object = ucl.loadClass(fileName).newInstance();
+            StringBuffer output = new StringBuffer("");
+            object.getClass().getDeclaredMethod("execute", output.getClass()).invoke(object, output);
+            System.out.println(output);
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException | FileNotFoundException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
