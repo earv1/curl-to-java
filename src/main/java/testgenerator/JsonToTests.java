b2f0cf42be67bf0fb5553cbb7aa964c6ffc4d2a3
Mano <airscythe@gmail.com>
2019-07-28 16:37:16 +0200

    Code for test generator

--- src/main/java/testgenerator/JsonToTests.java
+++ src/main/java/testgenerator/JsonToTests.java
@@ -4,36 +4,74 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class JsonToTests {
 
     final static ObjectMapper mapper = new ObjectMapper(); // create once, reuse
-    public static void jsonToTest(String json, String parent) {
+
+    public static void jsonToTest(String json) {
+
         try {
-
             JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
-            if(jsonNode.get(0).get("id").asInt() != 1) {
-                return;
-            }
-
-            if(!jsonNode.get(0).get("name").asText().equals("Leanne Graham")) {
-                return;
-            }
-
-            if(!jsonNode.get(0).get("address").get("street").asText().equals("Kulas Light")) {
-                return;
-            }
-            JsonNode s = jsonNode.get(0).get("id");
-
-            System.out.println(jsonNode);
-
-
+            jsonToTest(jsonNode, "jsonNode");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String getConversionString(JsonNode jsonNode){
+    private static String jsonToTest(JsonNode jsonNode, String parentNodeSelector) {
+//            if(jsonNode.get(0).get("id").asInt() != 1) {
+//                return "";
+//            }
+//
+//            if(!jsonNode.get(0).get("name").asText().equals("Leanne Graham")) {
+//                return "";
+//            }
+//
+//            if(!jsonNode.get(0).get("address").get("street").asText().equals("Kulas Light")) {
+//                return "";
+//            }
+
+            String generatedCodeString = "";
+            if(jsonNode.isArray()) {
+                int jsonElementIndex = 0;
+                for(JsonNode jsonNodeElement: jsonNode) {
+                   generatedCodeString += jsonToTest(jsonNode, parentNodeSelector + ".get(" + jsonElementIndex + ")");
+                   jsonElementIndex ++;
+                }
+            } else if (jsonNode.isValueNode()) {
+                Iterator<JsonNode> elements = jsonNode.elements();
+                while(elements.hasNext()) {
+                    JsonNode element = elements.next();
+                    generatedCodeString += jsonToTest(element, parentNodeSelector) + "\n";
+                }
+            } else {
+                generatedCodeString +=
+                        "if (!(" +
+                                "" +getConversionString(jsonNode) + getConversionString(jsonNode) +
+                                "" +generateEquals(jsonNode) + ")) {\n" +
+                                "   return false;\n" +
+                                "}";
+            }
+            return generatedCodeString;
+    }
+
+    public static String generateEquals(JsonNode jsonNode) {
+        if(jsonNode.isTextual()){
+            return ".equals(\"" + jsonNode.textValue() + "\")";
+        } else if(jsonNode.isBoolean()) {
+            return " == " + jsonNode.textValue();
+        } else if (jsonNode.isDouble()) {
+            return " == " + jsonNode.textValue();
+        } else if (jsonNode.isInt()) {
+            return " == " + jsonNode.textValue();
+        } else {
+            return "UNKNOWN CONVERSION";
+        }
+    }
+
+    public static String getConversionString(JsonNode jsonNode) {
         if(jsonNode.isTextual()){
             return ".asText()";
         } else if(jsonNode.isBoolean()) {
